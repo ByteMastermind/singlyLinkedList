669ec5cb8d45655d71b67998878cd3f2b37ecd72
michal <michal.benes10@gmail.com>
2021-11-07 19:38:04 +0100

    Remade to all self-destructive

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <stdexcept>
+#include <iostream>
 
 template<typename T>
 struct linkedListItem {
     linkedListItem * next;
-    T data;
+    T* data;
 };
 
 template<typename T>
@@ -15,13 +16,15 @@ class CLinkedList {
             while (this->first != nullptr) {
                 tmp = this->first;
                 this->first = this->first->next;
+                delete tmp->data;
                 delete tmp;
             }
         }
         void insertStart(const T x) {
             linkedListItem<T> * newItem = new linkedListItem<T>;
 
-            newItem->data = x;
+            newItem->data = new T;
+            *(newItem->data) = x;
             newItem->next = this->first;
 
             this->first = newItem;
@@ -31,13 +34,15 @@ class CLinkedList {
 
         void insertEnd(const T x) {
             linkedListItem<T> * newItem = new linkedListItem<T>;
-            newItem->data = x;
+            newItem->data = new T;
+            *(newItem->data) = x;
             newItem->next = nullptr;
 
             if (this->last)
                 this->last->next = newItem;
-            else
+            else {
                 this->first = newItem;
+            }
 
             this->last = newItem;
 
@@ -50,15 +55,17 @@ class CLinkedList {
             while (this->first != nullptr) {
                 tmp = this->first;
                 this->first = this->first->next;
+                delete tmp->data;
                 delete tmp;
             }
 
             this->first = nullptr;
             this->last = nullptr;
+            this->it = nullptr;
             this->itemCount = 0;
         }
 
-        T & at(const unsigned n) const { // Starting with 0
+        T * at(const unsigned n) const { // Starting with 0
             if (!itemCount || n >= itemCount)
                 throw std::invalid_argument("This item does not exist in singly linked list");
 
@@ -74,8 +81,47 @@ class CLinkedList {
             return itemCount;
         }
 
+        T * next() {
+            if (this->it == nullptr || this->it->next == nullptr)
+                this->it = this->first;
+            else
+                this->it = this->it->next;
+            return this->it->data;
+        }
+
+        void remove(const T* item) {
+            linkedListItem<T> * tmp = this->first;
+
+            if (tmp->data == item) {
+                if (itemCount == 1)
+                    this->last = nullptr;
+                this->first = tmp->next;
+                if (itemCount == 2)
+                    this->last = tmp;
+                delete tmp->data;
+                delete tmp;
+                itemCount--;
+                return;
+            }
+
+            unsigned counter = 0;
+            while (!(tmp->next->data == item)) {
+                tmp = tmp->next;
+                counter++;
+            }
+            linkedListItem<T> * nextnext = tmp->next->next;
+            delete tmp->next->data;
+            delete tmp->next;
+            if (counter == this->itemCount - 2)
+                tmp->next = nullptr;
+            else
+                tmp->next = nextnext;
+            itemCount--;
+        }
+
     private:
         linkedListItem<T> * first = nullptr;
         linkedListItem<T> * last = nullptr;
+        linkedListItem<T> * it = nullptr;
         unsigned itemCount = 0;
 };
